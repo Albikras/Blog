c185832c06ab601c2865d8146f52c514997f79d5
Albikras <124819029+Albikras@users.noreply.github.com>
2023-06-11 13:51:16 -0400

    added some code to the handle bars section

--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,7 @@
-const sequelize = require('../config/connection');
-const { User } = require('../models');
+const sequelize = require("../config/connection");
+const { User } = require("../models");
 
-const userData = require('./userData.json');
+const userData = require("./postData.json");
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -15,6 +15,3 @@ const seedDatabase = async () => {
 };
 
 seedDatabase();
-
-
-
